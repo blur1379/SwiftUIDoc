933ea62118bbf80abce1b58dc81d1962e581c9e5
blur1379 <77322707+blur1379@users.noreply.github.com>
2022-08-04 18:33:36 +0430

    Initial Commit

---
+++ iOSDoc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSDoc
+//
+//  Created by Mohammad Blur on 8/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSDoc/iOSDocApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDocApp.swift
+//  iOSDoc
+//
+//  Created by Mohammad Blur on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSDocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
